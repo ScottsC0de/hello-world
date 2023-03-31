14fe35e3a5f61567da65a921c2a8bd2ce06524ad
scottsc0de <scott5902@gmail.com>
2023-03-31 13:55:38 -0400

    resume component looking good

--- src/components/sections/Resume.js
+++ src/components/sections/Resume.js
@@ -1,29 +1,35 @@
 import React from 'react';
 
 function Resume() {
-    return (
-          <div className="sections">
-            <p>
-                You can download my awesome resume <a href='' download>here</a>
-                Or view it as a GitHub gist here: <a href=''></a>
-            </p>
-            <h3>Front-End</h3>
-            <ul>
-              <li>HTML // Markdown</li>
-              <li>CSS // Bootstrap</li>
-              <li>JavaScript // jQuery</li>
-              <li>React // WebPack</li>
-            </ul>
-            <h3>Back-End</h3>
-            <ul>
-              <li>Web APIs</li>
-              <li>Node.js // Express.js</li>
-              <li>MySQL // Sequelize</li>
-              <li>MongoDB // Mongoose</li>
-              <li>GraphQL</li>
-            </ul>
-          </div>
-      );
+  return (
+    <div className="sections">
+      <p>
+        You can download page 1 of my awesome resume <a href='public/assets/resume1.png' download>here</a>,
+        <br />
+        Page 2 right <a href='public/assets/resume2.png' download>here</a>
+        <br />
+        Or view it as a GitHub gist here: <a href='https://gist.github.com/ScottsC0de/2ade3611df58b6ad7b4c198aaa16eef4'>My Awesome Resume</a>
+      </p>
+      <br />
+      <h3>Front-End Artistries</h3>
+      <hr />
+      <ul>
+        <li>HTML // Markdown</li>
+        <li>CSS // Bootstrap</li>
+        <li>JavaScript // jQuery</li>
+        <li>React // WebPack</li>
+      </ul>
+      <h3>Back-End Efficiencies</h3>
+      <hr />
+      <ul>
+        <li>Web APIs</li>
+        <li>Node.js // Express.js</li>
+        <li>MySQL // Sequelize</li>
+        <li>MongoDB // Mongoose</li>
+        <li>GraphQL</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Resume;
