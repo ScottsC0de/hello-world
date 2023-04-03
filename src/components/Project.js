92c88659af1ad142d6f0ac70a0f2dd28d05c3190
scottsc0de <scott5902@gmail.com>
2023-04-03 18:25:51 -0400

    updated video links with music

--- src/components/Project.js
+++ src/components/Project.js
@@ -12,32 +12,32 @@ const [projects] = useState([
     {
         name: "Working Social Media API",
         description: "Social media api created using MongoDB, Mongoose ODM, & Express.js with JSON data storage and retrieval demonstrated using Insomnia",
-        link: "https://www.youtube.com/watch?v=_9QMaGcVVWM&t=127s",
+        link: "https://youtu.be/_9QMaGcVVWM",
         repo: "https://github.com/ScottsC0de/social-api",
-    },
-    {
-        name: "Employee Tracker",
-        description: "Application that allows you to track all employees in a database using mySQL and the inquirer npm package",
-        link: "https://www.youtube.com/watch?v=12fUfrv_vkg&t=17s",
-        repo: "https://github.com/ScottsC0de/employee-tracker",
     },
     {
         name: "E-Commerce Back-End",
         description: "Back-end application that uses the ORM Sequelize to communicate with a local database",
-        link: "https://www.youtube.com/watch?v=N2_ZViMa5r4&t=99s",
+        link: "https://youtu.be/suPdpcR-5OY",
         repo: "https://github.com/ScottsC0de/e-commerce",
+    },
+    {
+        name: "SQL Database Tracker",
+        description: "Application that allows you to track all employees in a database using mySQL and the inquirer npm package",
+        link: "https://youtu.be/jJTQS-aa_f0",
+        repo: "https://github.com/ScottsC0de/employee-tracker",
     },
     {
         name: "myTeam App",
         description: "Generate an HTML page for your software team",
-        link: "https://www.youtube.com/watch?v=a0Dp6FPVI6w&t=20s",
+        link: "https://youtu.be/JoxPK37G1w0",
         repo: "https://github.com/ScottsC0de/myteam-app",
     },
     {
         name: "ReadMe Machine",
         description: "Generate ReadMe files using node.js",
-        link: "https://www.youtube.com/watch?v=eNjHm8k3z60&t=13s",
-        repo: "https://github.com/ScottsC0de/readme-machineyou",
+        link: "https://youtu.be/FiDq-eSe_e0",
+        repo: "https://github.com/ScottsC0de/readme-machine",
     }
 ]);
 
